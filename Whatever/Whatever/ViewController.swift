e8df57da62a4ed22fc0c53dc762adeb67eddca96
Retno Widyanti <retno@retnowidyanti.com>
2019-09-07 12:05:54 +1000

    [FEAT] Initial commit. Created Xcode project and .gitignore.

---
+++ Whatever/Whatever/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whatever
+//
+//  Created by Retno Widyanti on 7/9/19.
+//  Copyright © 2019 Retno Widyanti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
